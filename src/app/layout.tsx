968bd4d8bd9fd103f54f86e3fd39ef848431706e
Javier Pérez García <javierez1998@gmail.com>
2026-01-04 21:16:37 +0100

    ñlkajsf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
         <CookieConsentBanner />
         <ServiceWorkerRegistration />
         <SpeedInsights />
-        <Analytics />
+        <Analytics /> 
       </body>
     </html>
   );
